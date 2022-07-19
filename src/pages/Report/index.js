47d056bc205ff2939d764abd5428d29c551b2471
SubhoBasak <subhobasak50@gmail.com>
2022-07-19 17:21:42 +0530

    updates

--- src/pages/Report/index.js
+++ src/pages/Report/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Table } from "react-bootstrap";
+import { Col, Row, Table } from "react-bootstrap";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -22,173 +22,127 @@ ChartJS.register(
 );
 
 const Report = () => {
-  const [janData, setJanData] = React.useState({ count: 0, amount: 0 });
-  const [febData, setFebData] = React.useState({ count: 0, amount: 0 });
-  const [marData, setMarData] = React.useState({ count: 0, amount: 0 });
-  const [aprData, setAprData] = React.useState({ count: 0, amount: 0 });
-  const [mayData, setMayData] = React.useState({ count: 0, amount: 0 });
-  const [junData, setJunData] = React.useState({ count: 0, amount: 0 });
-  const [julData, setJulData] = React.useState({ count: 0, amount: 0 });
-  const [augData, setAugData] = React.useState({ count: 0, amount: 0 });
-  const [sepData, setSepData] = React.useState({ count: 0, amount: 0 });
-  const [octData, setOctData] = React.useState({ count: 0, amount: 0 });
-  const [novData, setNovData] = React.useState({ count: 0, amount: 0 });
-  const [decData, setDecData] = React.useState({ count: 0, amount: 0 });
+  const [janData, setJanData] = React.useState(0);
+  const [febData, setFebData] = React.useState(0);
+  const [marData, setMarData] = React.useState(0);
+  const [aprData, setAprData] = React.useState(0);
+  const [mayData, setMayData] = React.useState(0);
+  const [junData, setJunData] = React.useState(0);
+  const [julData, setJulData] = React.useState(0);
+  const [augData, setAugData] = React.useState(0);
+  const [sepData, setSepData] = React.useState(0);
+  const [octData, setOctData] = React.useState(0);
+  const [novData, setNovData] = React.useState(0);
+  const [decData, setDecData] = React.useState(0);
 
   return (
     <div className="w-100 p-3">
       <div className="container">
-        <Table striped className="report-table p-3 border border-1 rounded">
-          <thead>
-            <tr>
-              <th>Month</th>
-              <th>Count</th>
-              <th>Amount</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>January</td>
-              <td>{janData.count}</td>
-              <td>{janData.amount}</td>
-            </tr>
-            <tr>
-              <td>February</td>
-              <td>{febData.count}</td>
-              <td>{febData.amount}</td>
-            </tr>
-            <tr>
-              <td>March</td>
-              <td>{marData.count}</td>
-              <td>{marData.amount}</td>
-            </tr>
-            <tr>
-              <td>April</td>
-              <td>{aprData.count}</td>
-              <td>{aprData.amount}</td>
-            </tr>
-            <tr>
-              <td>May</td>
-              <td>{mayData.count}</td>
-              <td>{mayData.amount}</td>
-            </tr>
-            <tr>
-              <td>June</td>
-              <td>{junData.count}</td>
-              <td>{junData.amount}</td>
-            </tr>
-            <tr>
-              <td>July</td>
-              <td>{julData.count}</td>
-              <td>{julData.amount}</td>
-            </tr>
-            <tr>
-              <td>August</td>
-              <td>{augData.count}</td>
-              <td>{augData.amount}</td>
-            </tr>
-            <tr>
-              <td>Spetember</td>
-              <td>{sepData.count}</td>
-              <td>{sepData.amount}</td>
-            </tr>
-            <tr>
-              <td>October</td>
-              <td>{octData.count}</td>
-              <td>{octData.amount}</td>
-            </tr>
-            <tr>
-              <td>November</td>
-              <td>{novData.count}</td>
-              <td>{novData.amount}</td>
-            </tr>
-            <tr>
-              <td>December</td>
-              <td>{decData.count}</td>
-              <td>{decData.amount}</td>
-            </tr>
-          </tbody>
-        </Table>
-        <Bar
-          className="mb-5"
-          options={{
-            responsive: true,
-            plugins: {
-              legend: {
-                position: "top",
-              },
-              title: {
-                display: true,
-                text: "Chart.js Bar Chart",
-              },
-            },
-          }}
-          data={{
-            labels: [
-              "January",
-              "February",
-              "March",
-              "April",
-              "May",
-              "June",
-              "July",
-              "August",
-              "September",
-              "October",
-              "November",
-              "December",
-            ],
-            datasets: [
-              {
-                label: "Sale",
-                data: [
-                  500, 1000, 700, 653, 234, 834, 434, 923, 742, 632, 874, 432,
+        <Row className="w-100">
+          <Col lg="3" md="12" sm="12">
+            <Table striped className="report-table p-3 border border-1 rounded">
+              <thead>
+                <tr>
+                  <th>Month</th>
+                  <th>Count</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>January</td>
+                  <td>{janData}</td>
+                </tr>
+                <tr>
+                  <td>February</td>
+                  <td>{febData}</td>
+                </tr>
+                <tr>
+                  <td>March</td>
+                  <td>{marData}</td>
+                </tr>
+                <tr>
+                  <td>April</td>
+                  <td>{aprData}</td>
+                </tr>
+                <tr>
+                  <td>May</td>
+                  <td>{mayData}</td>
+                </tr>
+                <tr>
+                  <td>June</td>
+                  <td>{junData}</td>
+                </tr>
+                <tr>
+                  <td>July</td>
+                  <td>{julData}</td>
+                </tr>
+                <tr>
+                  <td>August</td>
+                  <td>{augData}</td>
+                </tr>
+                <tr>
+                  <td>Spetember</td>
+                  <td>{sepData}</td>
+                </tr>
+                <tr>
+                  <td>October</td>
+                  <td>{octData}</td>
+                </tr>
+                <tr>
+                  <td>November</td>
+                  <td>{novData}</td>
+                </tr>
+                <tr>
+                  <td>December</td>
+                  <td>{decData}</td>
+                </tr>
+              </tbody>
+            </Table>
+          </Col>
+          <Col lg="9" md="12" sm="12">
+            <Bar
+              className="mb-5"
+              options={{
+                responsive: true,
+                plugins: {
+                  legend: {
+                    position: "top",
+                  },
+                  title: {
+                    display: true,
+                    text: "Chart.js Bar Chart",
+                  },
+                },
+              }}
+              data={{
+                labels: [
+                  "January",
+                  "February",
+                  "March",
+                  "April",
+                  "May",
+                  "June",
+                  "July",
+                  "August",
+                  "September",
+                  "October",
+                  "November",
+                  "December",
                 ],
-                backgroundColor: "rgba(255, 99, 132, 0.5)",
-              },
-            ],
-          }}
-        />
-        <Bar
-          className="mb-5"
-          options={{
-            responsive: true,
-            plugins: {
-              legend: {
-                position: "top",
-              },
-              title: {
-                display: true,
-                text: "Chart.js Bar Chart",
-              },
-            },
-          }}
-          data={{
-            labels: [
-              "January",
-              "February",
-              "March",
-              "April",
-              "May",
-              "June",
-              "July",
-              "August",
-              "September",
-              "October",
-              "November",
-              "December",
-            ],
-            datasets: [
-              {
-                label: "Sale",
-                data: [
-                  500, 1000, 700, 653, 234, 834, 434, 923, 742, 632, 874, 432,
+                datasets: [
+                  {
+                    label: "Sale",
+                    data: [
+                      500, 1000, 700, 653, 234, 834, 434, 923, 742, 632, 874,
+                      432,
+                    ],
+                    backgroundColor: "rgba(255, 99, 132, 0.5)",
+                  },
                 ],
-                backgroundColor: "rgba(53, 162, 235, 0.5)",
-              },
-            ],
-          }}
-        />
+              }}
+            />
+          </Col>
+        </Row>
       </div>
     </div>
   );
